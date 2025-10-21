8580c1079653596a111dc54c4c1fcaccbc76c397
Asya Yayla <149153342+Asyayla@users.noreply.github.com>
2025-10-21 20:46:46 +0300

    Update tailwind.config.js

--- building-management-frontend/tailwind.config.js
+++ building-management-frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // src klasöründeki tüm JS/TS/JSX/TSX dosyaları
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {},
